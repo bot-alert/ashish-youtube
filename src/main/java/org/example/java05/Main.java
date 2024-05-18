4eeff1d768ab2de1448edbae1a9438b1811d61a3
Ashish Khatiwada <ashishkahtiwada091@gmail.com>
2024-05-18 20:35:29 +0545

    java05 complete

---
+++ src/main/java/org/example/java05/Main.java
@@ -0,0 +1,29 @@
+package org.example.java05;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 19;
+        short s = 10;
+        byte b = 19;
+        long l = 19;
+
+
+        char c = 'A';
+
+        float f = 19.89f;
+        double d = 19.02d;
+
+
+        boolean isSunday = true;
+        boolean isMonday = false;
+
+        String name = "Ashish";
+        String name2 = "Ram";
+
+
+        System.out.println(name);
+        System.out.println(name2);
+
+
+    }
+}
