4dd4179d99f44564e4ab83fe4e8e65a0878b8c85
Ashish Khatiwada <ashishkahtiwada091@gmail.com>
2024-07-28 21:37:13 +0545

    java 7

---
+++ src/main/java/org/example/java07/Operators.java
@@ -0,0 +1,13 @@
+package org.example.java07;
+
+public class Operators {
+//    Arithmetic Operators -> + , - , * , / , ++ ,--
+//    Assignment Operators- > =, -=
+//    Comparison Operators -> == , > ,< ,>= ,<=
+//    Logical Operators -> && , ||, !
+//    Bitwise Operators -> &, |
+
+    public static void main(String[] args) {
+
+    }
+}
